9e5d282a7d6ab63ec2611f68b294827ef43c16d5
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-06 14:06:04 +0000

    3.6: Phonebook backend step6

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ let persons = [
   },
 ];
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
@@ -33,12 +35,12 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/info", (req, res) => {
-    const date = Date(Date.now());
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
-    ${date}`)
-  
-    res.status(204).end();
-  });
+  const date = Date(Date.now());
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    ${date}`);
+
+  res.status(204).end();
+});
 
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
@@ -51,13 +53,48 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+const generateId = () => {
+  const id = Math.floor(Math.random() * 999999);
+  return id;
+};
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: "name is missing",
+    });
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: "number is missing",
+    });
+  }
+
+  if(persons.map(person => person.name === body.name)){
+    return res.status(400).json({
+      error:"name must be unique"
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
 
   res.status(204).end();
 });
-
 
 const PORT = 3001;
 app.listen(PORT, () => {
