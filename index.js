0e7dd5e8733cb6cef2bff7d190de37a059abdc85
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-07 20:21:52 +0000

    3.22: Lint configuration

--- index.js
+++ index.js
@@ -1,77 +1,76 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/person");
-const { response } = require("express");
-app.use(express.static("build"));
-app.use(express.json());
-app.use(cors());
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :log")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :log')
+)
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   Person.find({}).then((persons) => {
-    res.json(persons);
-  });
-});
+    res.json(persons)
+  })
+})
 
-app.get("/info", (req, res) => {
-  const date = Date(Date.now());
+app.get('/info', (req, res) => {
+  const date = Date(Date.now())
   res.send(`<p>Phonebook has info for ${Person.length} people</p>
-  ${date}`);
+  ${date}`)
 
-  res.status(204).end();
-});
+  res.status(204).end()
+})
 
-app.get("/api/persons/:id", (req, res,next) => {
+app.get('/api/persons/:id', (req, res,next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (req, res,next) => {
-  const body = req.body;
+app.post('/api/persons', (req, res,next) => {
+  const body = req.body
 
   if (!body.name) {
     return res.status(400).json({
-      error: "name is missing",
-    });
+      error: 'name is missing',
+    })
   }
 
   if (!body.number) {
     return res.status(400).json({
-      error: "number is missing",
-    });
+      error: 'number is missing',
+    })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      res.json(savedPerson);
+      res.json(savedPerson)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
-app.put("/api/persons/:id",(req,res,next) => {
-  const body = req.body;
+app.put('/api/persons/:id',(req,res,next) => {
+  const body = req.body
   const person = {
     name: body.name,
     number: body.number,
@@ -82,40 +81,40 @@ app.put("/api/persons/:id",(req,res,next) => {
       res.json(updatePerson)
     })
     .catch(error => next(error))
-});
+})
 
-app.delete("/api/persons/:id", (req, res, next) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-morgan.token("log", function (req, res) {
-  return JSON.stringify(req.body);
-});
+morgan.token('log', function (req) {
+  return JSON.stringify(req.body)
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
 
-  if (error.name === "CastError" && error.kind == "ObjectId") {
-    return response.status(400).send({ error: "id doesn't exist" });
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'id doesn\'t exist' })
   }
   else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
 
   next(error)
-};
+}
 
-app.use(errorHandler);
-const PORT = process.env.PORT;
+app.use(errorHandler)
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
