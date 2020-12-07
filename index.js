11f28d82f38395e0bf1c29742a9172f50c1d7fa5
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-07 15:26:41 +0000

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -67,9 +67,12 @@ app.post("/api/persons", (req, res) => {
     });
 });
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+
+  Person.findByIdAndRemove(req.params.id)
+  .then(result => {
+    res.status(204).end()
+  })
+  .catch(err => next(error))
 });
 
 morgan.token("log", function (req, res) {
